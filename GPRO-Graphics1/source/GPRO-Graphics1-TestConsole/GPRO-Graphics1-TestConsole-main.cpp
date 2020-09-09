958fa4f01738044b44ccfc4f8b5ee0aadfee60e0
Justin Marko <justin.marko@mymail.champlain.edu>
2020-09-09 10:55:16 -0400

    Added chapter 4.2

--- GPRO-Graphics1/source/GPRO-Graphics1-TestConsole/GPRO-Graphics1-TestConsole-main.cpp
+++ GPRO-Graphics1/source/GPRO-Graphics1-TestConsole/GPRO-Graphics1-TestConsole-main.cpp
@@ -30,6 +30,7 @@
 #include <string>
 #include <vec3.h>
 #include <color.h>
+#include <ray.h>
 
 //#include "gpro/gpro-math/gproVector.h"
 
@@ -57,12 +58,31 @@ using namespace std;
 //#endif	// __cplusplus
 //}
 
+color ray_color(const ray& r) 
+{
+	vec3 unit_direction = unit_vector(r.direction());
+	double t = 0.5 * (unit_direction.y() + 1.0);
+	return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
+}
+
 int main(int const argc, char const* const argv[])
 {
 	//testVector();
 
-	const int image_width = 256;
-	const int image_height = 256;
+	const double aspect_ratio = 16.0 / 9.0;
+	const int image_width = 400;
+	const int image_height = static_cast<int>(image_width / aspect_ratio);
+
+	// Camera
+
+	double viewport_height = 2.0;
+	double viewport_width = aspect_ratio * viewport_height;
+	double focal_length = 1.0;
+
+	vec3 origin = point3(0, 0, 0);
+	vec3 horizontal = vec3(viewport_width, 0, 0);
+	vec3 vertical = vec3(0, viewport_height, 0);
+	vec3 lower_left_corner = origin - horizontal / 2 - vertical / 2 - vec3(0, 0, focal_length);
 
 	ofstream file("output.ppm");
 
@@ -73,7 +93,10 @@ int main(int const argc, char const* const argv[])
 		cout << "Lines remaining " << j << endl;
 		for (int i = 0; i < image_width; ++i) 
 		{
-			color pixel_color(double(i) / (image_width - 1), double(j) / (image_height - 1), 0.25);
+			double u = double(i) / (image_width - 1);
+			double v = double(j) / (image_height - 1);
+			ray r(origin, lower_left_corner + u * horizontal + v * vertical - origin);
+			color pixel_color = ray_color(r);
 			write_color(file, pixel_color);
 		}
 	}
